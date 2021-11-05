07e327bb55c6dc511c26c3d4a3ea0cb9ae9872fa
Alexey <shchukin2002@yandex.ru>
2021-11-05 04:30:00 +0300

    First look of site

---
+++ FashionProject/FashionProject/Controllers/RegistrationLoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FashionProject.Controllers
+{
+    public class RegistrationLoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        
+    }
+}
